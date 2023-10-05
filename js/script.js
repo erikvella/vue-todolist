4152a0e791cb8d9283c97e59afca3c97a6b0a33d
erikvella <vella.erik1@gmail.com>
2023-10-05 16:01:34 +0100

    [fix] removeTask

--- js/script.js
+++ js/script.js
@@ -4,39 +4,59 @@ createApp({
   data(){
     return{
        tasks:[
-        'Fare la spesa',
-        'Studiare Vue',
-        'Giocare a D2r',
+        {
+          text: 'Fare la spesa',
+          done: false
+  
+         },
+         {
+          text: 'Studiare Vue',
+          done: false
+         },
+         {
+          text: 'Giocare a D2r',
+          done: true
+         },
+        
        ],
+       
+        
+       
        newTask : '',
-       isError : false,
-       doneTask : '',
-       isDone : false
+       errorMsg: '',
+    
     }
   },
   methods:{
     addTask(){
 
       if(this.newTask.length < 5){
-        this.isError = true;
+
       }else{
         console.log(this.newTask);
         this.tasks.unshift(this.newTask);
         this.newTask ='';
-        this.isError = false;
+  
          
       }
    
     },
 
     removeTask(index){
-          this.tasks.splice(index , 1)
+      if(this.tasks[index].done){
+
+        this.tasks.splice(index,1)
+      }else{
+        this.writeErrors('Errore! Non hai ancora svolto questo task!') ; 
+      }
     },
 
-    markTask(){
-      if(this.newTask.includes(doneTasks)){
+    writeErrors(errorStr){
+       this.errorMsg = errorStr;
+       setTimeout(()=>{
+          this.errorMsg = '';
+       },2000)
 
-      }
     }
  
   },
